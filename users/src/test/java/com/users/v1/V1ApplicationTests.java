9876373048aebdd5b33a9313ddeba78e314d33be
felipematheus1337 <lipehbr@gmail.com>
2026-03-11 14:18:09 -0300

    project created.

---
+++ users/src/test/java/com/users/v1/V1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.users.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
